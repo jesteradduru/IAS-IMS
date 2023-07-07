e14be002cacbab25491d9afe1de317b106ee9d1b
Jester <jesteradduru@gmail.com>
2023-07-07 22:25:48 +0800

    Setup laravel project, jetstream + inertia vue

---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+            <div>
+                <span class="text-4xl font-black">IAS </span>
+                <span class="text-4xl">Inspection and Monitoring System</span>
+            </div>
+
+            <h1 class="mt-8 text-2xl font-medium text-gray-900">
+                Welcome to your IAS-IMS!
+            </h1>
+
+            <p class="mt-6 text-gray-500 leading-relaxed">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, similique officiis obcaecati reprehenderit harum deleniti rem et ut vitae error aliquid minima. Voluptates atque maiores ut sed nulla dolorum itaque quae esse aliquid. Maiores ex dicta sunt eaque iure et reprehenderit quod cum voluptate dignissimos totam, dolor aperiam corporis beatae.
+            </p>
+        </div>
+    </div>
+</template>
