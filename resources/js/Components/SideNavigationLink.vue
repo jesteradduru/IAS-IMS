5b8b4f47c82bce174668c3c96129054c74b931a9
Jester Adduru <jesteradduru@gmail.com>
2023-08-22 14:12:44 +0800

    updated side nav link

--- resources/js/Components/SideNavigationLink.vue
+++ resources/js/Components/SideNavigationLink.vue
@@ -1,13 +1,14 @@
 <template>
-    <a v-if="route().current(link)" :href="route(link)" class="p-2 text-sm rounded-md bg-indigo-700 text-gray-50 shadow-md hover:bg-indigo-600 cursor-pointer">
+    <Link v-if="route().current(link)" :href="route(link)" class="p-2 text-sm rounded-md bg-indigo-700 text-gray-50 shadow-md hover:bg-indigo-600 cursor-pointer">
         <slot />
-    </a>
-    <a v-else :href="route(link)" class="p-2 text-sm rounded-md bg-indigo-500 text-gray-50 shadow-md hover:bg-indigo-600 cursor-pointer">
+    </Link>
+    <Link v-else :href="route(link)" class="p-2 text-sm rounded-md bg-indigo-500 text-gray-50 shadow-md hover:bg-indigo-600 cursor-pointer">
         <slot />
-    </a>
+    </Link>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3';
 defineProps({
     link: String
 })
